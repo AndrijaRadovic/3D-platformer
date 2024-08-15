806f7aea6423f86d5fb36211b1255e7689d3aead
AndrijaRadovic <andrijaa1radovic@gmail.com>
2024-08-16 00:59:40 +0200

    Doradjeno kretanje igraca, dodate pokretne platforme, dodati protivnici i sva propratna logika

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float x;
+    [SerializeField] float y;
+    [SerializeField] float z;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(x, y, z);
+    }
+}
---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private bool dead = false;
+
+    private void Update()
+    {
+        if (transform.position.y < -1.5f && !dead)
+        {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Invoke(nameof(ReloadLevel), 1.5f);
+        dead = true;
+    }
+
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
